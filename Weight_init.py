1d123b8178ca973a5f068d8181a5825c934c0b21
choujujua <52094265+choujujua@users.noreply.github.com>
2020-07-13 20:48:20 +0800

    Create Weight_init.py
    pytorch 自定义网络权重初始化

---
+++ Weight_init.py
@@ -0,0 +1,9 @@
+    #pytorch 自定义网络权重初始化
+    def _init_weight(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
