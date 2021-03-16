98eae3c8465f8cd4d2cdeb708d81e9bb8941a90f
choujujua <52094265+choujujua@users.noreply.github.com>
2021-03-16 16:28:31 +0800

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,1 +1,88 @@
 1.压缩文件为 tar -zxvf 
+1.压缩文件为 tar -zxvf 
+
+
+Keras style model.summary() in PyTorch
+
+Usage 安装
+1.pip install torchsummary or
+2.git clone https://github.com/sksq96/pytorch-summary
+  
+使用
+from torchsummary import summary
+summary(your_model, input_size=(channels, H, W))
+
+
+Example：CNN for MNIST
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchsummary import summary
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
+        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        x = F.relu(F.max_pool2d(self.conv1(x), 2))
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
+        x = x.view(-1, 320)
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu") # PyTorch v0.4.0
+model = Net().to(device)
+
+summary(model, (1, 28, 28))
+
+结果
+----------------------------------------------------------------
+        Layer (type)               Output Shape         Param #
+================================================================
+            Conv2d-1           [-1, 10, 24, 24]             260
+            Conv2d-2             [-1, 20, 8, 8]           5,020
+         Dropout2d-3             [-1, 20, 8, 8]               0
+            Linear-4                   [-1, 50]          16,050
+            Linear-5                   [-1, 10]             510
+================================================================
+Total params: 21,840
+Trainable params: 21,840
+Non-trainable params: 0
+----------------------------------------------------------------
+Input size (MB): 0.00
+Forward/backward pass size (MB): 0.06
+Params size (MB): 0.08
+Estimated Total Size (MB): 0.15
+----------------------------------------------------------------
+
+Multiple Inputs
+
+import torch
+import torch.nn as nn
+from torchsummary import summary
+
+class SimpleConv(nn.Module):
+    def __init__(self):
+        super(SimpleConv, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(1, 1, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+        )
+
+    def forward(self, x, y):
+        x1 = self.features(x)
+        x2 = self.features(y)
+        return x1, x2
+    
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model = SimpleConv().to(device)
+
+summary(model, [(1, 16, 16), (1, 28, 28)])
